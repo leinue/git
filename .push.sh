355ff262b2c7cb0f0d519986d8bfcd2542705b78
leinue <597055914@qq.com>
2016-11-16 15:36:17 +0800

    updae

--- .push.sh
+++ .push.sh
@@ -1,1 +1,1 @@
-git --git-dir=/root/workspace/.git --work-tree=/root/workspace add . && git --git-dir=/root/workspace/.git --work-tree=/root/workspace commit -a -m 'update'
+git --git-dir=/root/workspace/.git --work-tree=/root/workspace add push -u origin master
